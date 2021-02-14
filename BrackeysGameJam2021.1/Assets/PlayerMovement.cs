5ab71c5e88c81a2baa6ee218c0f818cbcfd7e2c4
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-02-14 10:59:31 -0500

    add current project progress

---
+++ BrackeysGameJam2021.1/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float walkSpeed;
+
+	public bool invertHorizontalInput; // bool that controlls if the lef and right inputs are inverted
+	private float horizontalMultiplier;
+	//public bool invertVerticalInput; // probably don't need this
+
+
+	private Rigidbody2D rb;
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		if (invertHorizontalInput) horizontalMultiplier = -1;
+		else horizontalMultiplier = 1;
+	}
+
+	void FixedUpdate()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal") * horizontalMultiplier;
+
+		rb.velocity = new Vector2(horizontalInput * walkSpeed, rb.velocity.y);
+	}
+}
---
+++ BrackeysGameJam2021.1/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+	public int playerID; // 1 for player one, 2 for p
+
+    public GameObject playerOnePrefab; //player prefab To spawn when the game starts and when the player respawns
+    public GameObject playerTwoPrefab;
+
+    void Start()
+    {
+    	SpawnPlayer(playerID);
+    }
+
+    public void SpawnPlayer (int ID)
+    {
+    	switch (ID) 
+    	{
+    		case 0:
+    		  
+    		  	break;
+    		case 1:
+    		   
+    			break;
+    	}
+    }
+}
